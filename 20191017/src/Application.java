836e74308771ac3ef7083e72b2b02ed6a242ab42
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-17 21:06:49 +0200

    Added question 4 and answer

--- 20191017/src/Application.java
+++ 20191017/src/Application.java
@@ -94,7 +94,12 @@ public class Application {
 
         // zad 4
         // napraviti tablicu mnozenja 10x10
-        
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
