c7db4387b5797a5ce8fad52203ff147c30a1975f
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-17 20:27:02 +0200

    updated gitignore

---
+++ 20191017/src/Application.java
@@ -0,0 +1,28 @@
+public class Application {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int a = 5;
+        System.out.println("varijabla a = " + a);
+
+        double b = 3.14;
+        System.out.println("b = " + b);
+
+        int c = (int) (a + b);
+        System.out.println("c = " + c);
+
+        boolean bool = false;
+        System.out.println("bool = " + bool);
+
+        short s = (short) 33000;
+        System.out.println("s = " + s);
+
+        char character = '#';
+        System.out.println(character);
+
+        String str = "Algebra";
+        System.out.println(str);
+    }
+
+}
