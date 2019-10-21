f463172eb29c82cef83592009e050d6381dbae76
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-21 20:18:17 +0200

    Added for loop example - number multiplication

---
+++ 20191021/src/ForLoopExample1.java
@@ -0,0 +1,10 @@
+public class ForLoopExample1 {
+    // Napisati program koji ispisuje prirodne brojeve (od 1 do 20) i njihove kvadrate (for petlja).
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i + "x" + i + " = " + i * i);
+        }
+
+    }
+}
