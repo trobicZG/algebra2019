79be0d4d8c72b19dbdd1566cf160dfbfd5e676e0
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-21 18:56:42 +0200

    Added another scanner example

---
+++ 20191021/src/Examples.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Examples {
+
+    // Napišite program koji će u prvom redu ispisati vaše ime i prezime,
+    // nakon toga jedan red prazno i u trećem redu naziv mjesta u kojem živite.
+
+    // Zatim te podatke ispisati u jednoj liniji odvojene povlakom ( - )
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ime: ");
+        String firstName = scanner.next();
+
+        System.out.println("Prezime: ");
+        String lastName = scanner.next();
+
+        System.out.println();
+        System.out.println("Grad: ");
+        String city = scanner.next();
+
+        System.out.println(firstName + " - " + lastName + " - " + city);
+    }
+}
