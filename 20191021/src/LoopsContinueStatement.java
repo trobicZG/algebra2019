8213c8b22e63f8075bbeee84598250cc5ca9fb66
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-21 20:46:52 +0200

    Added example for break statement

---
+++ 20191021/src/LoopsContinueStatement.java
@@ -0,0 +1,12 @@
+public class LoopsContinueStatement {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 17 == 0) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
