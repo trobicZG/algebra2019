b750f5b7513d3a829cf55bc33644f58a73edb8c8
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-21 19:08:01 +0200

    calculating hypotenuse - v1

---
+++ 20191021/src/TriangleCalculationVersion1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TriangleCalculationVersion1 {
+
+    // Napišite program izračunati duljinu hipotenuze pravokutnog trokuta
+    // ako imamo zadane duljine kateta a i b.
+    // Hipotenuza se računa pomoću Pitagorinog poučka: c2 = a2 + b2
+    // a i b stranice unosi korisnik kroz konzolu
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("a: ");
+        int a = scanner.nextInt();
+
+        System.out.println("b:");
+        int b = scanner.nextInt();
+
+        int result = a * a + b * b;
+
+        System.out.println("c2 =  " + result);
+    }
+}
