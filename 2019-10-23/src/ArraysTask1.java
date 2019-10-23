e320268012602adb0f0bf246f3cdd779a53f19b6
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-23 18:28:42 +0200

    Added task 1 for arrays

--- 2019-10-23/src/ArraysTask1.java
+++ 2019-10-23/src/ArraysTask1.java
@@ -1,2 +1,14 @@
 public class ArraysTask1 {
+    public static void main(String[] args) {
+
+        int numbers[] = new int[100];
+
+        for (int i = 1; i <= 100; i++) {
+            numbers[i - 1] = i;
+        }
+
+        for (int n : numbers) {
+            System.out.println(n);
+        }
+    }
 }
