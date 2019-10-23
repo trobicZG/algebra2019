bdd3bdcd146c5e983dbc287d5e6f062188329445
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-23 20:39:01 +0200

    Showed string comparison and continuing with custom functions

--- 2019-10-23/src/Functions1.java
+++ 2019-10-23/src/Functions1.java
@@ -3,17 +3,70 @@ public class Functions1 {
 
         String date = "2019-10-23";
 
-        if (getMonth(date).equals("10")) {
-            System.out.println("Listopad");
+        int month = getMonth(date);
+
+        if (month > 6) {
+            System.out.println("Proslo je pola godine.");
         } else {
-            System.out.println("Nije listopad");
+            System.out.println("U prvoj polovici godine smo.");
         }
 
+        boolean b1 = isEqual("algebra", "Algebra");
+        boolean b2 = isEqual("algebra", "algebra");
+        boolean b3 = isEqual("algebra ", "algebra");
+        boolean b4 = isEqual(" algebra", "algebra");
+        boolean b5 = isEqual(new String("algebra"), "algebra");
+
+        String s1 = "Algebra";
+        String s2 = "Algebra";
+
+        boolean b10 = s1.equals(s2);
+        boolean b11 = s2.equals(s1);
+        boolean b12 = s1.equals("Algebra");
+        boolean b13 = s2.equals("Algebra");
+        boolean b14 = s1.equals(new String("Algebra"));
+        boolean b15 = s2.equals(new String("Algebra"));
+
+        String s3 = s2;
+        boolean b20 = s1.equals(s2);
+        boolean b21 = s1.equals(s3);
+        s2 = "Nesto drugo";
+        boolean b22 = s1.equals(s2);
+        boolean b23 = s1.equals(s3);
+
+        String s5 = "algebra";
+        String s6 = "algebra";
+        String s7 = s6;
+
+        boolean b30 = s5 == s6;
+        boolean b31 = s5 == s7;
+        s6 = "Nesto drugo";
+        boolean b32 = s5 == s6;
+        boolean b33 = s5 == s7;
+        boolean b34 = s5 == new String("algebra");
+
+        printCharacters(20);
+        System.out.println();
+        System.out.println();
+        printCharacters(50);
     }
 
-    static String getMonth(String date) {
+    static Integer getMonth(String date) {
         String[] arr = date.split("-");
-        return arr[1];
+        return Integer.parseInt(arr[1]);
+    }
+
+    static boolean isEqual(String first, String second) {
+        if (first == second) {
+            return true;
+        }
+        return false;
+    }
+
+    static void printCharacters(int howMany) {
+        for (int i = 0; i < howMany; i++) {
+            System.out.println("*");
+        }
     }
 
 }
