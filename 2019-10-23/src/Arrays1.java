ac7c4269b08efe145eacd99485db340349a81c67
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-23 18:12:25 +0200

    Arrays example - reading with FOR loop

---
+++ 2019-10-23/src/Arrays1.java
@@ -0,0 +1,15 @@
+public class Arrays1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[20];
+
+        for (int i = 1; i<=20; i++) {
+            numbers[i-1] = i;
+        }
+
+        for (int i = 0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
