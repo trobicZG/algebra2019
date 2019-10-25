f585296c44242a9e8e5b28a4a4a6b32facdc0874
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-25 18:41:19 +0200

    Added task 2 - square drawing

--- 2019-10-25/src/Task2Square.java
+++ 2019-10-25/src/Task2Square.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task2Square {
+    // Nacrtati kvadrat u konozlu. Korisnik odreduje dimenzije.
+
+    public static void main(String[] args) {
+
+        System.out.println("Dimenzije kvadrata: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        drawSquare(size);
+
+    }
+
+    private static void drawSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
