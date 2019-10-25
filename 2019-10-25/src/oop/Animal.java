bb83624e2c232030174518368c1b36e78e6921e9
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-25 20:32:59 +0200

    Added getters and setters

--- 2019-10-25/src/oop/Animal.java
+++ 2019-10-25/src/oop/Animal.java
@@ -13,4 +13,28 @@ public class Animal {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public int getLegsCount() {
+        return legsCount;
+    }
+
+    public void setLegsCount(int legsCount) {
+        this.legsCount = legsCount;
+    }
+
+    public int getTeethCount() {
+        return teethCount;
+    }
+
+    public void setTeethCount(int teethCount) {
+        this.teethCount = teethCount;
+    }
 }
