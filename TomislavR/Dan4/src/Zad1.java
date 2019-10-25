c09315940c20270b6fa74534eb0ec4909b2a1d9c
revolutionsoftcro <revolutionsoft.cro@gmail.com>
2019-10-25 18:15:53 +0200

    Dan4 Zad1

--- TomislavR/Dan4/src/Zad1.java
+++ TomislavR/Dan4/src/Zad1.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 public class Zad1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("height: ");
+        int height = scanner.nextInt();
+        int blank=height-1;
+        int stars=1;
+        for (int i=0; i<height; i++) {
+            for(int j=0; j<blank; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<stars; k++) {
+                System.out.print("*");
+            }
+            blank-=1;
+            stars+=2;
+            System.out.println();
+        }
     }
 }
