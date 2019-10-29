5700972c79927e0035219f3059737dfd3434d930
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-29 18:22:37 +0100

    OOP projects starting - Vehicles

---
+++ 2019-10-29/src/Car.java
@@ -0,0 +1,11 @@
+public class Car extends Vehicle {
+    private Double doorsCount; // broj vrata
+
+    public Double getDoorsCount() {
+        return doorsCount;
+    }
+
+    public void setDoorsCount(Double doorsCount) {
+        this.doorsCount = doorsCount;
+    }
+}
