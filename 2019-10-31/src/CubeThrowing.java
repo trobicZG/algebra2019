bd638a223dfd9722366f05e023f966d5259eb5f7
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-31 18:58:31 +0100

    introduced static final variables for min and max values

--- 2019-10-31/src/CubeThrowing.java
+++ 2019-10-31/src/CubeThrowing.java
@@ -1,10 +1,13 @@
 import java.util.Random;
 
 public class CubeThrowing {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 6;
+
     static int getRandomNumber() {
         Random r = new Random();
 
-        return r.nextInt((6 - 1) + 1 ) + 1;
+        return r.nextInt((MAX_VALUE - MIN_VALUE) + 1) + MIN_VALUE;
     }
 
 }
