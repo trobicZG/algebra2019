df6b938ecb13e9f0c054eab475640c1bffcd9b84
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-31 19:34:21 +0100

    Generating number can now accept MIN and MAX values

--- 2019-10-31/src/Application2.java
+++ 2019-10-31/src/Application2.java
@@ -1,13 +1,17 @@
 import java.util.HashMap;
 
 public class Application2 {
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 6;
+
     public static void main(String[] args) {
+
         // CUBE THROWING
 
         HashMap<Integer, Integer> counterMap = new HashMap<>();
 
         for (int i = 0; i < 999; i++) {
-            int randomNumber = CubeThrowing.getRandomNumber();
+            int randomNumber = CubeThrowing.getRandomNumber(MIN_VALUE, MAX_VALUE);
 
             if (counterMap.containsKey(randomNumber)) {
                 counterMap.put(randomNumber, counterMap.get(randomNumber) + 1);
--- 2019-10-31/src/CubeThrowing.java
+++ 2019-10-31/src/CubeThrowing.java
@@ -1,13 +1,10 @@
 import java.util.Random;
 
 public class CubeThrowing {
-    private static final int MIN_VALUE = 1;
-    private static final int MAX_VALUE = 6;
-
     private final static Random r = new Random();
 
-    static int getRandomNumber() {
-        return r.nextInt((MAX_VALUE - MIN_VALUE) + 1) + MIN_VALUE;
+    static int getRandomNumber(int min, int max) {
+        return r.nextInt((max - min) + 1) + min;
     }
 
 }
