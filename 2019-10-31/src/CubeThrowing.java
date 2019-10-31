6028a71668d244048b930c086ca8967869c3e689
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-31 19:01:31 +0100

    Moved random to be static. Before it was making new Random object for each loop itteration

--- 2019-10-31/src/CubeThrowing.java
+++ 2019-10-31/src/CubeThrowing.java
@@ -4,9 +4,9 @@ public class CubeThrowing {
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 6;
 
-    static int getRandomNumber() {
-        Random r = new Random();
+    private final static Random r = new Random();
 
+    static int getRandomNumber() {
         return r.nextInt((MAX_VALUE - MIN_VALUE) + 1) + MIN_VALUE;
     }
 
