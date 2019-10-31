aa566037285098254d225eacb831b5bb8887790f
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-10-31 19:28:04 +0100

    Added solution with map

--- 2019-10-31/src/Application2.java
+++ 2019-10-31/src/Application2.java
@@ -1,27 +1,24 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Application2 {
     public static void main(String[] args) {
         // CUBE THROWING
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
-        list.add(0);
-        list.add(0);
-        list.add(0);
-        list.add(0);
-        list.add(0);
+        HashMap<Integer, Integer> counterMap = new HashMap<>();
 
         for (int i = 0; i < 999; i++) {
             int randomNumber = CubeThrowing.getRandomNumber();
 
-            list.set(randomNumber - 1, list.get(randomNumber - 1) + 1);
+            if (counterMap.containsKey(randomNumber)) {
+                counterMap.put(randomNumber, counterMap.get(randomNumber) + 1);
+            } else {
+                counterMap.put(randomNumber, 1);
+            }
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(i + 1 + ": " + list.get(i));
+        for (int i = 1; i <= 6; i++) {
+            System.out.println(i + ": " + counterMap.get(i));
         }
-
 
     }
 }
