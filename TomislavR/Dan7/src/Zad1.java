8f99274267a216ec6aeeb093facb27555ee8f68f
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-04 18:29:00 +0100

    Dan7 Zad1 fix

--- TomislavR/Dan7/src/Zad1.java
+++ TomislavR/Dan7/src/Zad1.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 
 public class Zad1 {
     public static void main(String[] args) {
-        final int MAX_NUMBER=6;
-        Scanner scanner=new Scanner(System.in);
-        int[] array=new int[MAX_NUMBER];
-        for (int i=0; i<MAX_NUMBER; i++) {
+        final int MAX_NUMBER = 6;
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[MAX_NUMBER];
+        for (int i = 0; i < MAX_NUMBER; i++) {
             try {
-                array[i]=scanner.nextInt();
+                System.out.println("Enter number: ");
+                array[i] = scanner.nextInt();
             } catch (InputMismatchException e) {
-                System.out.println(e.getMessage());
-                continue;
+                scanner.next();
+                System.out.println("Wrong input!");
+                i--;
             }
         }
-        for (int i=0; i<MAX_NUMBER; i++)
+        for (int i = 0; i < MAX_NUMBER; i++)
             System.out.println(array[i]);
 
     }
