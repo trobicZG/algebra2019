9c3b4fe2cfe300ed89ea705faaf8dc24f2a01f90
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-04 19:17:09 +0100

    Shortened the code

--- 2019-11-04/src/ReadFromFileExample.java
+++ 2019-11-04/src/ReadFromFileExample.java
@@ -9,11 +9,10 @@ public class ReadFromFileExample {
         final String fileToRead = "file1.txt";
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(fileToRead));
-        String line = bufferedReader.readLine();
+        String line;
 
-        while (line != null) {
+        while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
-            line = bufferedReader.readLine();
         }
     }
 }
