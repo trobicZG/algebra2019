129c008296767a0ac3752924cdb0c86c7a4ba923
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-04 19:52:29 +0100

    Added support for adding new students

---
+++ 2019-11-04/src/namebook/Application.java
@@ -0,0 +1,56 @@
+package namebook;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exitApplication = false;
+        while (!exitApplication) {
+            showMainMenu();
+            try {
+                int option = scanner.nextInt();
+
+                if (option != 0 && option != 1 && option != 2) {
+                    throw new IllegalArgumentException("Invalid option");
+                }
+
+                if (option == 0) {
+                    break;
+                }
+
+                if (option == 1) {
+                    Student s = new Student();
+
+                    System.out.println("First name: ");
+                    s.setFirstName(scanner.next());
+
+                    System.out.println("Last name: ");
+                    s.setLastName(scanner.next());
+
+                    System.out.println("ID number: ");
+                    String tmp = scanner.next();
+                    try {
+                        s.setId(Long.parseLong(tmp));
+                    } catch (NumberFormatException e) {
+                        throw new IllegalArgumentException("ID must contain only numbers.");
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid option");
+                scanner.next();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void showMainMenu() {
+        System.out.println("Choose your option:");
+        System.out.println("0 -> Exit application");
+        System.out.println("1 -> Add new student");
+        System.out.println("2 -> Show all students");
+    }
+}
