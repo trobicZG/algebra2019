78c801be38de16bd9886f22a265b5b81c5eb9020
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-06 19:57:22 +0100

    Gym App

--- TomislavR/Dan8/src/Gym.java
+++ TomislavR/Dan8/src/Gym.java
@@ -1,2 +1,86 @@
+import java.util.Scanner;
+
 public class Gym {
+    private int ID;
+    private String workingHours;
+    private String address;
+    private String city;
+    private String phoneNumber;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return this.ID + "," + this.workingHours + "," + this.address + "," + this.city + "," + this.phoneNumber;
+    }
+
+    public static Gym fromConsole() {
+        Scanner scanner = new Scanner(System.in);
+        Gym g = new Gym();
+
+        System.out.println("Working hours: ");
+        g.setWorkingHours(scanner.next());
+
+        System.out.println("Address: ");
+        g.setAddress(scanner.next());
+
+        System.out.println("City: ");
+        g.setCity(scanner.next());
+
+        System.out.println("Phone: ");
+        g.setPhoneNumber(scanner.next());
+
+        return g;
+    }
+
+    public static Gym fromString(String input) {
+        String[] values = input.split(",");
+        Gym g = new Gym();
+        g.setID((int) Long.parseLong(values[0]));
+        g.setWorkingHours(values[1]);
+        g.setAddress(values[2]);
+        g.setPhoneNumber(values[3]);
+        g.setPhoneNumber(values[4]);
+
+        return g;
+    }
+
 }
--- TomislavR/Dan8/src/GymApplication.java
+++ TomislavR/Dan8/src/GymApplication.java
@@ -1,6 +1,87 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
+
 public class GymApplication {
 
-    public static void main(String[] args) {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Map<Integer, String> allowedOptionsMap = new HashMap<>();
+    private static Map<Integer, String> allowedOptions2Map = new HashMap<>();
 
+    private static final int EXIT_APPLICATION_CODE = 0;
+    private static final int GYM_CODE = 1;
+    private static final int MEMBERS_CODE = 2;
+    private static final int FEE_CODE = 3;
+    private static final int EQUIPMENT_CODE = 4;
+    private static final int ADD_GYM_CODE = 1;
+    private static final int SHOW_ALL_GYMS_CODE = 2;
+
+    public static void main(String[] args) throws IOException {
+        populateAllowedOptions();
+        populateAllowedOptions2();
+
+        while (true) {
+            System.out.println(" Main menu :");
+            showMainMenu();
+            try {
+                int option = scanner.nextInt();
+
+                if (!allowedOptionsMap.containsKey(option)) {
+                    throw new InvalidOptionException(option);
+                }
+
+                if (option == EXIT_APPLICATION_CODE) {
+                    break;
+                }
+
+                if (option == GYM_CODE) {
+                    while (true) {
+                        System.out.println(" Gym menu :");
+                        showGymMenu();
+                        option = scanner.nextInt();
+
+                        if (option == ADD_GYM_CODE) {
+                            Gym gym=Gym.fromConsole();
+                            ReadWriteUtil.saveToTxt(gym);
+                        }
+                    }
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid option");
+                scanner.next();
+            } catch (IllegalArgumentException | InvalidOptionException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
+
+    private static void showMainMenu() {
+        for (int i = 0; i < allowedOptionsMap.size(); i++) {
+            System.out.println(i + " -> " + allowedOptionsMap.get(i));
+        }
+    }
+
+    private static void showGymMenu() {
+        for (int i = 1; i <= allowedOptions2Map.size(); i++) {
+            System.out.println(i + " -> " + allowedOptions2Map.get(i));
+        }
+    }
+
+    private static void populateAllowedOptions() {
+        allowedOptionsMap.put(EXIT_APPLICATION_CODE, "Exit application.");
+        allowedOptionsMap.put(GYM_CODE, "Gym.");
+        allowedOptionsMap.put(MEMBERS_CODE, "Members.");
+        allowedOptionsMap.put(FEE_CODE, "Fee.");
+        allowedOptionsMap.put(EQUIPMENT_CODE, "Equipment.");
+    }
+
+    private static void populateAllowedOptions2() {
+        allowedOptions2Map.put(ADD_GYM_CODE, "Add gym.");
+        allowedOptions2Map.put(SHOW_ALL_GYMS_CODE , "Show all gyms.");
+    }
+
+
 }
