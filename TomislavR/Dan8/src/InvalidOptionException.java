fa8166b90465c8b99616dc7a8e153a1c326bec88
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-06 20:01:46 +0100

    Gym App update1

---
+++ TomislavR/Dan8/src/InvalidOptionException.java
@@ -0,0 +1,12 @@
+public class InvalidOptionException extends RuntimeException {
+    private String message;
+
+    public InvalidOptionException(int option) {
+        this.message = "ERROR: " + option + " is not a valid option!";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ TomislavR/Dan8/src/ReadWriteUtil.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ReadWriteUtil {
+
+    public static void saveToTxt(Gym g) throws IOException {
+        File f = new File("gyms.txt");
+        int ID;
+        if (f.exists()) {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("students.txt"));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Gym g1 = Gym.fromString(line);
+                g.setID(g1.getID());
+            }
+        }
+        else {
+            g.setID(1);
+        }
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(f, true));
+        bufferedWriter.write(g.toString());
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+    }
+
+    public static ArrayList<Gym> toStudentsList() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("students.txt"));
+        ArrayList<Gym> gyms = new ArrayList<>();
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            Gym g = Gym.fromString(line);
+            gyms.add(g);
+        }
+        return gyms;
+    }
+}
