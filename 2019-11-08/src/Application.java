bf38c4a735292b90ef578ad0cecd770cbfb722a2
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-08 19:24:28 +0100

    syntax error correction in main()

--- 2019-11-08/src/Application.java
+++ 2019-11-08/src/Application.java
@@ -1,9 +1,6 @@
-import model.TransportVehicle;
-import model.Vehicle;
-import model.WeaponizedVehicle;
-
 public class Application {
 
     public static void main(String[] args) {
-
+        
+    }
 }
