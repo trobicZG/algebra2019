0fc3481aa2b135c4c4886f22bf61c5bcb10714dd
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-12 18:34:39 +0100

    Shown difference between set and list

---
+++ 2018-11-12/src/Application.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(2);
+
+        Collections.sort(arrayList);
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(1);
+        hashSet.add(2);
+        hashSet.add(3);
+        hashSet.add(2);
+
+        Person p1 = new Person("Sasa", "Fajkovic", 30);
+        Person p2 = new Person("Maja", "Majic", 31);
+        Person p3 = new Person("Sasa", "Fajkovic", 30);
+
+        Set<Person> personSet = new HashSet<>();
+        personSet.add(p1);
+        personSet.add(p2);
+        personSet.add(p3);
+
+    }
+}
