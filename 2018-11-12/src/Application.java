c9cea6c5829b6088d9c426b90971a1df982e93e9
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-12 19:48:54 +0100

    Added more properties to Person

--- 2018-11-12/src/Application.java
+++ 2018-11-12/src/Application.java
@@ -46,7 +46,5 @@ public class Application {
 
         Object[] personArrayList = personSet.toArray();
         Arrays.sort(personArrayList);
-
-
     }
 }
--- 2018-11-12/src/Person.java
+++ 2018-11-12/src/Person.java
@@ -1,12 +1,41 @@
 public class Person implements Comparable<Person> {
+    private Long id;
     private String firstName;
     private String lastName;
     private int age;
+    private String address;
+    private String phoneNumber;
 
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, String lastName, int age, String address, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getFirstName() {
