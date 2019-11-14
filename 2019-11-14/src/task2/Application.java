076f44520bc101c2bf5ed98cf85ceeda22a31090
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-14 19:14:16 +0100

    format fix

--- 2019-11-14/src/task2/Application.java
+++ 2019-11-14/src/task2/Application.java
@@ -11,7 +11,7 @@ public class Application {
         int size = scanner.nextInt();
 
         for (int i = 1; i <= size; i++) {
-            for (int j = 1; j<=size; j++ ) {
+            for (int j = 1; j <= size; j++) {
                 System.out.print(i * j + " ");
             }
             System.out.println();
