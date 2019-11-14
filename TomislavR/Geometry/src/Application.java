3a13a833a4860db4d1290a59fc0d8a39ef04867f
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-14 20:29:04 +0100

    Geometry fix

--- TomislavR/Geometry/src/Application.java
+++ TomislavR/Geometry/src/Application.java
@@ -27,7 +27,7 @@ public class Application {
         int count = 1;
         for ( int i = 0; i < height; i++) {
             for ( int j=0; j < count; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
             count++;
