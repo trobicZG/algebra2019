b6735427b483b05232629c378e1ebf7f0551bfe9
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-18 19:14:13 +0100

    OsobeStarost zadatak

---
+++ TomislavR/OsobeStarost/src/Application.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+        String EXIT_INPUT = "x";
+        String name = "a";
+        int age = 0;
+        ArrayList<Person> persons = new ArrayList<Person>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("  Unesite ime ili x za završetak unosa: ");
+            name = scanner.nextLine();
+            if (name.equals(EXIT_INPUT))
+                break;
+            while (true) {
+                System.out.print( " Unesite broj godina: ");
+                try {
+                    age = Integer.parseInt(scanner.nextLine());
+                    break;
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println( "Krivi unos broja godina, probajte opet!");
+                }
+            }
+
+            Person person = new Person(name, age);
+            persons.add(person);
+        }
+
+        Collections.sort(persons);
+        System.out.println( "Osobe poredane po dobi :");
+        for (int i=0; i<persons.size(); i++) {
+            System.out.println(persons.get(i).getName() + "  " + persons.get(i).getAge());
+        }
+    }
+
+}
