7cbb61605227c08be3c5c130a5f895d478c9e834
Tomislav Robic <tomislavrobic.pmf@gmail.com>
2019-11-18 18:32:16 +0100

    Zadatak raspon brojeva fix2

--- TomislavR/BrojeviRaspon/src/Application.java
+++ TomislavR/BrojeviRaspon/src/Application.java
@@ -4,26 +4,25 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-        int firstNumber, secondNumber, x;
+        int firstNumber = 0, secondNumber = 0, x = 1;
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        boolean check = true;
+        while (check) {
             try {
-                while (true) {
-                    System.out.print(" Unesite prvi broj raspona :");
-                    firstNumber = Integer.parseInt(scanner.nextLine());
-                    System.out.print(" Unesite drugi broj raspona :");
-                    secondNumber = Integer.parseInt(scanner.nextLine());
-                    if (firstNumber > secondNumber) {
-                        System.out.println(" Krivi unos raspona, pokušajte ponovo!");
-                    } else {
-                        break;
-                    }
-
+                System.out.print(" Unesite prvi broj raspona :");
+                firstNumber = Integer.parseInt(scanner.nextLine());
+                System.out.print(" Unesite drugi broj raspona :");
+                secondNumber = Integer.parseInt(scanner.nextLine());
+                if (firstNumber > secondNumber) {
+                    System.out.println(" Krivi unos raspona, pokušajte ponovo!");
+                } else {
+                    check = false;
+                }
+                if (!check) {
+                    System.out.print(" Unesite broj za djeljivost :");
+                    x = Integer.parseInt(scanner.nextLine());
                 }
 
-                System.out.print(" Unesite broj za djeljivost :");
-                x = Integer.parseInt(scanner.nextLine());
-                break;
             } catch (IllegalArgumentException e) {
                 System.out.println("Niste unjeli cijele brojeve, pokušajte ponovo!");
             }
@@ -32,7 +31,7 @@ public class Application {
         int counter = 0;
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         for (int i=firstNumber; i<=secondNumber; i++) {
-            if (i%x == 0) {
+            if (i % x == 0) {
                 ++counter;
                 numbers.add(i);
             }
