0444b6841fa84f0da8348f511abff07ae18d5495
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-18 21:02:39 +0100

    completed task 3 - students and grades

---
+++ 2019-11-18/src/task3/StudentMapper.java
@@ -0,0 +1,26 @@
+package task3;
+
+public abstract class StudentMapper {
+
+    public static Student toStudent(String string) {
+        String[] data = string.split(Constants.DATA_SEPARATOR);
+        return new Student(
+                data[Constants.FIRST_NAME_INDEX],
+                data[Constants.LAST_NAME_INDEX],
+                Integer.parseInt(data[Constants.AGE_INDEX]),
+                Integer.parseInt(data[Constants.GRADE_INDEX]));
+    }
+
+    public static String toString(Student student) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(student.getFirstName());
+        sb.append(Constants.DATA_SEPARATOR);
+        sb.append(student.getLastName());
+        sb.append(Constants.DATA_SEPARATOR);
+        sb.append(student.getAge());
+        sb.append(Constants.DATA_SEPARATOR);
+        sb.append(student.getGrade());
+
+        return sb.toString();
+    }
+}
