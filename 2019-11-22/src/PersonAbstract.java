85411349b77fbab758f57bdeea48a20172bbf3c0
Sasa Fajkovic <fajkovic.sasa@gmail.com>
2019-11-22 19:03:42 +0100

    made classes abstract

--- 2019-11-22/src/PersonAbstract.java
+++ 2019-11-22/src/PersonAbstract.java
@@ -1,4 +1,20 @@
-public class PersonAbstract {
+public abstract class PersonAbstract {
     protected String firstName;
     protected String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
